07ab5956b8fde5dc6b0fbe761028bc9e0aa9fe0b
synergy2411 <synergy2411@outlook.com>
2018-12-15 10:11:15 +0530

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
+import { Geolocation } from '@ionic-native/geolocation';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private geolocation : Geolocation, 
+              private camera : Camera,
+              private alertCtrl : AlertController){}
 
+  openCamera(){
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options)
+      .then((img)=>{
+        const alert = this.alertCtrl.create({
+          title : "Camera",
+          message : img,
+          buttons : [{
+            text : "OK"
+          }]
+        })
+        alert.present();
+        console.log(img);
+      })
+  }
+
+  getLocation() {
+
+    this.geolocation.getCurrentPosition()
+      .then((position)=>{
+
+        console.log("Latitude : " + position.coords.latitude + 
+                "\nLongitude : " + position.coords.longitude)
+      }).catch(err=>console.log(err))
   }
 
 }
